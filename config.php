6dd77c9d013d7ff016d7f95823bc5af7f924efc5
Aidos-act <aidos_h@yahoo.com>
2026-05-22 10:09:13 +0900

    try setting base to root

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copy this file to config.php and set your mail addresses.
+ */
+return [
+    // Admin inbox for trial applications
+    'mail_to' => 'aidos.activision@gmail.com',
+
+    // Envelope / From address (must be allowed on your host)
+    'mail_from' => 'noreply@kinfaku.jp',
+    'mail_from_name' => '金融ファクシミリ新聞社',
+
+    // Send confirmation email to the applicant
+    'send_autoreply' => true,
+];
